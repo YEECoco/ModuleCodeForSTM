755cdd6a0e129a9416218c85fd35122cb00e92e1
YHQ <as114514hshhz@163.com>
2025-02-21 19:52:50 +0800

    key

---
+++ 3.Key/key.c
@@ -0,0 +1,7 @@
+#include <key.h>
+#include "stdint.h"
+
+// HAL_GPIO_EXTI_Callback function rewrite
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+
+}
---
+++ 3.Key/key.h
@@ -0,0 +1,8 @@
+#ifndef BSP_KEY_H_
+#define BSP_KEY_H_
+
+/**
+ * include files placed here 
+ */
+
+#endif
